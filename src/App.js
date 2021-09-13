7dc8a65e605ca6742ad340d7343c6abd546fd418
ezecarpin <ezecarpin@hotmail.com>
2021-09-13 15:23:17 -0300

    app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import Quote from './components/Quote'
+
+const initialQuote = {
+  text: 'Quote',
+  author: 'autor'
+}
 
 function App() {
+
+  const [quote, setQuote] = useState(initialQuote)
+  const [loading, setLoading] = useState(false)
+
+  const updateQuote = async () => {
+    setLoading(true)
+    const url = "https://www.breakingbadapi.com/api/quote/random"
+    const res = await fetch(url)
+    const [newQuote] = await res.json()
+
+    const { quote, author } = newQuote
+    
+    setQuote({
+      text: quote,
+      author
+    })
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    updateQuote()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="app">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/7/77/Breaking_Bad_logo.svg"
+           alt="logo" />
+      <button onClick={() => updateQuote()}>Get Another</button>
+        { loading ? <h1>Loading...</h1> : <Quote quote={quote} /> }
+    </div>  
+  )
 }
 
 export default App;
